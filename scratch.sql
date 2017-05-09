f631ffa9696515b276f7405e0c54aeea4a0c1695
Joshua Huang <jhuang@101875mbp15.corp.qualys.com>
2017-05-09 10:17:37 -0700

    Updated scratch files

--- scratch.sql
+++
@@ -1,3 +0,0 @@
--- ADD JP description for blank_count widget template (AV-854)
-UPDATE widget_template SET DESCRIPTION = replace(DESCRIPTION, '<value lang="EN">', '<value lang="JA"></value><value lang="EN">') WHERE PREVIEW_CLS = 'blank_count';
-
