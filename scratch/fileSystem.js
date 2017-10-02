90d624ee061738cd8f3f5a97a6c8eca0905be2fb
Joshua Huang <jphuangjr@gmail.com>
2017-10-01 23:31:05 -0700

    Merged

--- scratch/fileSystem.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var jsonData = {"test": "hello"};
-fs.writeFile("/Users/jhuang/Projects/functionLibrary/test", JSON.stringify(jsonData), function(err) {
-  if(err) {
-    return console.log(err);
-  }
-
-  console.log("The file was saved!");
-});
